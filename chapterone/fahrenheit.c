33d800ee5954ae5bc12a5e3815b5f6cf3d241f3a
notypicalus <notypicalus@gmail.com>
2019-09-03 11:42:44 +0300

    1.2 fahrenheit conversion formula exercise

---
+++ chapterone/fahrenheit.c
@@ -0,0 +1,20 @@
+ 1 #include <stdio.h>
+
+/* print Fahrenheit-Celsius table
+for fahr = 0, 20, ..., 300 */
+int main()
+{
+    float fahr, celsius;
+    int lower, upper, step;
+    lower = 0;
+    upper = 300;
+    step = 20;
+    
+    fahr = upper
+    while (fahr <= upper)
+    {
+	celsius = 5 * (fahr, celsius);
+	printf("%d\t%d\n", fahr, celsius);
+	fahr = fahr + step;
+    }
+}
