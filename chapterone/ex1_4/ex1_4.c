6087987a501cf14c278e05525eb0447ee1227a35
notypicalus <notypicalus@gmail.com>
2019-09-12 12:16:20 +0300

    3.0f, 6.1f more accurate

--- chapterone/ex1_4/ex1_4.c
+++ chapterone/ex1_4/ex1_4.c
@@ -16,7 +16,7 @@ int main()
     while(fahr <= upper)
     {
 	celsius = (5.0 / 9.0) * (fahr-32.0);
-	printf("%f\t%f\n", fahr, celsius);
+	printf("%3.0f\t%6.1f\n", fahr, celsius);
 	fahr = fahr + step;
     }
 }
