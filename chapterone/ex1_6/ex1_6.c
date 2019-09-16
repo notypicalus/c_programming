e6024b6fa56698a0be7e2f595892cbda70126e82
notypicalus <notypicalus@gmail.com>
2019-09-16 14:41:19 +0300

    ex1_6.c file

---
+++ chapterone/ex1_6/ex1_6.c
@@ -0,0 +1,15 @@
+/*Verify that the expression getchar() != EOF is 0 or 1.*/
+
+#include <stdio.h>
+
+int main()
+{
+    int c;
+
+    while ((c = getchar()) != EOF)
+    {
+	printf("%d", c != EOF);
+	putchar(c);
+    }
+    printf("\n%d\n", c != EOF);
+}
