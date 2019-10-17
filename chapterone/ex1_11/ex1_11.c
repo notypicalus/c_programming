ff06c406120aaa9f6e6e09aa1bc3e7d8e479ec12
notypicalus <notypicalus@gmail.com>
2019-10-17 14:14:42 +0300

    ex1_11: c file

---
+++ chapterone/ex1_11/ex1_11.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#define IN 1
+#define OUT 0
+
+/* How would you test the word count program?
+ * What kinds of input are most likely to uncover bugs if there are any?
+ */
+
+int main()
+{
+    int c, nl, nw, nc, state;
+
+    state = OUT;
+    nl = nw = nc = 0;
+    while((c = getchar()) != EOF)
+	    {
+	    ++nc;
+	    if (c == '\n')
+	    ++nl;
+	    if (c == ' ' || c == '\n' || c == '\t')
+	    state = OUT;
+	    else if (state == OUT)
+	    {
+	    state = IN;
+	    ++nw;
+	    }
+	    }
+	    printf("%d %d %d\n", nl, nw, nc);
+}
